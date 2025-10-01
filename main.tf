0e54112a2db47e617df0883319be5ceb6e76b24b
Vladimir Joshevski Public <vjoshevs@cisco.com>
2025-10-01 10:43:16 +0200

    Fix module source path for sdwan and update version

--- main.tf
+++ main.tf
@@ -5,9 +5,7 @@
 # }
 
 module "sdwan" {
-  source = "github.com/netascode/terraform-sdwan-nac-sdwan"
-
+  source =  = "netascode/nac-sdwan/sdwan"
+  version          = "1.2.0"
   yaml_directories = ["data/"]
-
-  write_default_values_file = "defaults.yaml"
 }
