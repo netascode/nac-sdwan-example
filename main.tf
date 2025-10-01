619219a291bf9cd8251e8f94e104aa4f7094a09e
Vladimir Joshevski Public <vjoshevs@cisco.com>
2025-10-01 11:22:36 +0200

    Fix syntax error in module source declaration

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 # }
 
 module "sdwan" {
-  source =  = "netascode/nac-sdwan/sdwan"
+  source = "netascode/nac-sdwan/sdwan"
   version          = "1.2.0"
   yaml_directories = ["data/"]
   write_default_values_file = "defaults.yaml"
